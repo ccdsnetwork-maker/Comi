a1c28334aefe0b496c35bae2c55d29aef8a42b0c
ccdsnetwork-maker <ccdsnetwork@gmail.com>
2026-08-26 20:22:43 +0100

    prioritize base64 firebase key

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -4,7 +4,12 @@ import { getFirestore } from "firebase-admin/firestore"
 const projectId = process.env.FIREBASE_PROJECT_ID
 const clientEmail = process.env.FIREBASE_CLIENT_EMAIL
 
-let privateKey = process.env.FIREBASE_PRIVATE_KEY
+let privateKey = process.env.FIREBASE_PRIVATE_KEY_BASE64
+  ? Buffer.from(
+      process.env.FIREBASE_PRIVATE_KEY_BASE64,
+      "base64"
+    ).toString("utf-8")
+  : process.env.FIREBASE_PRIVATE_KEY
 
 if (!privateKey && process.env.FIREBASE_PRIVATE_KEY_BASE64) {
   privateKey = Buffer.from(
